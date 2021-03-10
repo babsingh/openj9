6d426a8fe9903f7661d2e4d28db9fd191ce1477f
Babneet Singh <sbabneet@ca.ibm.com>
2021-03-10 08:55:31 -0800

    Fix MethodHandleResolver
    Signed-off-by: Babneet Singh <sbabneet@ca.ibm.com>

--- jcl/src/java.base/share/classes/java/lang/invoke/MethodHandleResolver.java
+++ jcl/src/java.base/share/classes/java/lang/invoke/MethodHandleResolver.java
@@ -393,32 +393,36 @@ final class MethodHandleResolver {
 			String name,
 			String typeDescriptor,
 			ClassLoader loader) throws Throwable {
-		MethodType type = null;
 /*[IF OPENJDK_METHODHANDLES]*/
-		switch (cpRefKind) {
-		case 1: /* getField */
-		case 2: /* getStatic */
-		case 3: /* putField */
-		case 4: /* putStatic */
-			type = MethodTypeHelper.vmResolveFromMethodDescriptorString("(" + typeDescriptor + ")V", loader, null);
-			break;
-		case 5: /* invokeVirtual */
-		case 6: /* invokeStatic */
-		case 7: /* invokeSpecial */
-		case 8: /* newInvokeSpecial */
-		case 9: /* invokeInterface */
-			type = MethodTypeHelper.vmResolveFromMethodDescriptorString(typeDescriptor, loader, null);
-			break;
-		default:
-			/*[MSG "K0686", "Unknown reference kind: '{0}'"]*/
-			throw new InternalError(com.ibm.oti.util.Msg.getString("K0686", cpRefKind)); //$NON-NLS-1$
-		}
+                Object type = null;
+                MethodType mt = null;
+                switch (cpRefKind) {
+                case 1: /* getField */
+                case 2: /* getStatic */
+                case 3: /* putField */
+                case 4: /* putStatic */
+                        mt = MethodTypeHelper.vmResolveFromMethodDescriptorString("(" + typeDescriptor + ")V", loader, null);
+                        type = mt.parameterType(0);
+                        break;
+                case 5: /* invokeVirtual */
+                case 6: /* invokeStatic */
+                case 7: /* invokeSpecial */
+                case 8: /* newInvokeSpecial */
+                case 9: /* invokeInterface */
+                        mt = MethodTypeHelper.vmResolveFromMethodDescriptorString(typeDescriptor, loader, null);
+                        type = mt;
+                        break;
+                default:
+                        /*[MSG "K0686", "Unknown reference kind: '{0}'"]*/
+                        throw new InternalError(com.ibm.oti.util.Msg.getString("K0686", cpRefKind)); //$NON-NLS-1$
+                }
 
-		final MethodHandles.Lookup lookup = new MethodHandles.Lookup(currentClass);
-		inaccessibleTypeCheck(lookup, type);
-		
-		return MethodHandleNatives.linkMethodHandleConstant(currentClass, cpRefKind, referenceClazz, name, type);
+                final MethodHandles.Lookup lookup = new MethodHandles.Lookup(currentClass);
+                inaccessibleTypeCheck(lookup, mt);
+
+                return MethodHandleNatives.linkMethodHandleConstant(currentClass, cpRefKind, referenceClazz, name, type);
 /*[ELSE] OPENJDK_METHODHANDLES*/
+                MethodType type = null;
 		try {
 			MethodHandles.Lookup lookup = new MethodHandles.Lookup(currentClass, false);
 			MethodHandle result = null;
