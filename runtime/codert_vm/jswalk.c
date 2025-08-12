44a7833c51b019ae0c48bbc208dcb52f171b2b5a
Graham Chapman <graham_chapman@ca.ibm.com>
2025-08-11 21:58:12 -0400

    ROM Method Cache
    Signed-off-by: Graham Chapman <graham_chapman@ca.ibm.com>

--- runtime/codert_vm/jswalk.c
+++ runtime/codert_vm/jswalk.c
@@ -153,6 +153,8 @@ UDATA  jitWalkStackFrames(J9StackWalkState *walkState)
 	U_8 ** returnTable;
 	void  (*savedDropToCurrentFrame)(struct J9StackWalkState * walkState) ;
 
+	memset(&walkState->romMethodInfo, 0, sizeof(walkState->romMethodInfo));
+
 	if (J9_ARE_ANY_BITS_SET(walkState->flags, J9_STACKWALK_RESUME)) {
 		walkState->flags &= ~J9_STACKWALK_RESUME;
 		if (0 != walkState->inlineDepth) {
@@ -212,6 +214,8 @@ UDATA  jitWalkStackFrames(J9StackWalkState *walkState)
 						lswRecord(walkState, LSW_TYPE_METHOD, walkState->method);
 						lswRecord(walkState, LSW_TYPE_JIT_FRAME_INFO, walkState);
 #endif
+						initializeBasicROMMethodInfo(walkState, J9_ROM_METHOD_FROM_RAM_METHOD(walkState->method));
+
 						if ((rc = walkFrame(walkState)) != J9_STACKWALK_KEEP_ITERATING) {
 							return rc;
 						}
@@ -250,6 +254,7 @@ resumeWalkInline:
 #ifdef J9VM_INTERP_LINEAR_STACKWALK_TRACING
 		lswRecord(walkState, LSW_TYPE_JIT_FRAME_INFO, walkState);
 #endif	 
+		initializeBasicROMMethodInfo(walkState, J9_ROM_METHOD_FROM_RAM_METHOD(walkState->method));
 		if ((rc = walkFrame(walkState)) != J9_STACKWALK_KEEP_ITERATING) {
 			return rc;
 		}
@@ -1977,6 +1982,9 @@ jitWalkOSRFrame(J9StackWalkState *walkState, J9OSRFrame *osrFrame)
 	UDATA *localSlots = ((UDATA*)(osrFrame + 1)) + maxStack;
 	UDATA *nextFrame = localSlots + numberOfLocals;
 	J9MonitorEnterRecord *enterRecord = osrFrame->monitorEnterRecords;
+	J9ROMMethod *romMethod = J9_ROM_METHOD_FROM_RAM_METHOD(method);
+
+	initializeBasicROMMethodInfo(walkState, romMethod);
 
 #ifdef J9VM_INTERP_STACKWALK_TRACING
 	{
@@ -1987,6 +1995,7 @@ jitWalkOSRFrame(J9StackWalkState *walkState, J9OSRFrame *osrFrame)
 		walkState->method = stateMethod;
 	}
 #endif
+
 	walkBytecodeFrameSlots(walkState, method, offsetPC,
 			localSlots - 1, pendingStackHeight,
 			nextFrame - 1, numberOfLocals, TRUE);
